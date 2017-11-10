8d6a75e2f41f138fcce807fc9954082af2fe7054
Artyom <jemik.mail@gmail.com>
2017-11-10 19:12:28 +0300

    Initial commit

---
+++ src/Parser.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+class Parser {
+    private static Map<String,Integer> result;
+
+    Parser(){
+        result = new HashMap<>();
+    }
+
+    public void countJavaWords(String javaReservedWorlds, String inputFile, String outputFile){
+        try (OutputStream fileToWriteIn = new FileOutputStream(outputFile);
+             InputStream javaWords = new FileInputStream(javaReservedWorlds);
+             InputStream fileToReadFrom = new FileInputStream(inputFile))
+        {
+             byte[] bytesStreamJavaWords = new byte[javaWords.available()];
+             byte[] bytesStreamInputFile = new byte[fileToReadFrom.available()];
+             javaWords.read(bytesStreamJavaWords, 0, bytesStreamJavaWords.length);
+             fileToReadFrom.read(bytesStreamInputFile, 0, bytesStreamInputFile.length);
+
+             String javaWords_String = new String(bytesStreamJavaWords);
+             String[] arrayOfReservedWords = javaWords_String.split("\\n");
+
+             String fileToReadFrom_String = new String(bytesStreamInputFile);
+             String[] arrayInputFile = fileToReadFrom_String.split("\\s|\\W|\\D]");
+
+            for (int i = 0; i < arrayOfReservedWords.length ; i++) {
+                int count = 0;
+                for (String anArrayInputFile : arrayInputFile) {
+                    if (arrayOfReservedWords[i].equals(anArrayInputFile)) {
+                        result.put(arrayOfReservedWords[i], count++);
+                    }
+                }
+            }
+
+            String[] result_String = new String[result.size()];
+            int index = 0;
+            for (Map.Entry entry : result.entrySet()){
+                result_String[index] = "Word " + entry.getKey() + " was used in file "
+                        + inputFile
+                        + entry.getValue() + " times.";
+                index++;
+            }
+
+            StringBuilder stringBuilder= new StringBuilder();
+            for (String aResult_String : result_String) {
+                stringBuilder.append(aResult_String);
+            }
+
+            byte[] buffer = stringBuilder.toString().getBytes();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bos.write(buffer);
+            bos.writeTo(fileToWriteIn);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
